c8d0ddb6ec12aec345960ff26ef2522407a0ae7b
ebauschatz <eaenglish113@gmail.com>
2022-08-22 12:51:59 -0500

    docs: adding comments for future functionality structure

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ import console_display
 def main():
     cards_per_hand = 5
     console_display.display_welcome_message(cards_per_hand)
+    #generate deck
+    #shuffle deck
+    #deal hands to players
+    #determine pairs per hand
+    #display hands and pairs per hand
+    #determine and display the winner(s)
 
 main()
