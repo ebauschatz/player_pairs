64ad426904655dc612dac87a89abf876adfbf88f
ebauschatz <eaenglish113@gmail.com>
2022-08-22 13:32:06 -0500

    feat: adding new deck generation

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import console_display
 
 def main():
     cards_per_hand = 5
+    number_of_each_type_in_deck = 4
     console_display.display_welcome_message(cards_per_hand)
-    #generate deck
+    deck = generate_deck(number_of_each_type_in_deck)
     #shuffle deck
     #deal hands to players
     #determine pairs per hand
     #display hands and pairs per hand
     #determine and display the winner(s)
+
+def generate_deck(type_repetitions):
+    card_types = ['ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king']
+    new_deck = []
+    new_deck.extend(card_types * type_repetitions)
+    return new_deck
 
 main()
