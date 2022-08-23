9040881de0c0929c7a2615c5948036a719593982
ebauschatz <eaenglish113@gmail.com>
2022-08-23 10:39:58 -0500

    feat: added ability to select number of players

--- game_settings.py
+++ game_settings.py
@@ -5,7 +5,11 @@ def get_number_of_each_type_in_deck():
     return 4
 
 def get_number_of_players():
-    return 4
+    valid_input = False
+    while valid_input is False:
+        players = input('Please enter the number of players for this game: ')
+        if players.isnumeric() is True:
+            return int(players)
 
 def get_card_types():
     return ['ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king']
