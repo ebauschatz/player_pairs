bef1b0dc7155f160db2ea97fc372fdd8ed5fc39f
ebauschatz <eaenglish113@gmail.com>
2022-08-23 16:50:01 -0500

    test: tests for hand.find_max_value_from_key

--- test_hand.py
+++ test_hand.py
@@ -32,3 +32,30 @@ def test_track_wins_no_hands():
     
     hand.track_wins(hands)
     assert hands == hands_expected
+
+def test_find_max_value_from_key_single_option():
+    test_dicts = [
+        {'key1': 5, 'key2': 3}
+    ]
+
+    assert hand.find_max_value_from_key('key2', test_dicts) == 3
+
+def test_find_max_value_from_key_multiple_options():
+    test_dicts = [
+        {'key1': 5, 'key2': 3},
+        {'key1': 5, 'key2': 5},
+        {'key1': 5, 'key2': 8},
+        {'key1': 5, 'key2': 2}
+    ]
+
+    assert hand.find_max_value_from_key('key2', test_dicts) == 8
+
+def test_find_max_value_from_key_all_options_same_value():
+    test_dicts = [
+        {'key1': 5, 'key2': 4},
+        {'key1': 5, 'key2': 4},
+        {'key1': 5, 'key2': 4},
+        {'key1': 5, 'key2': 4}
+    ]
+
+    assert hand.find_max_value_from_key('key2', test_dicts) == 4
